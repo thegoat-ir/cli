064253faad9471348c3099892d4879b7a6a94c35
Mahdyar Hasanpour <hi@mahdyar.me>
2021-04-06 13:13:42 +0430

    fix: fix thegoat update (#6)

--- update.js
+++ update.js
@@ -4,7 +4,7 @@ const { exec } = require("child_process");
 
 const update = {
   update() {
-    exec("npm update thegoat", (error, stdout, stderr) => {
+    exec("npm update -g thegoat", (error, stdout, stderr) => {
       if (error) {
         console.log(`Error: ${error.message}`);
         return;
